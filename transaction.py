a34bf9bd7ad02e2d6dea7fc5f2a4791fbb4ae1d1
michalbaldyga <michal.baldyga01@gmail.com>
2025-01-13 14:11:23 +0100

    SBS-3 - add comments

--- transaction.py
+++ transaction.py
@@ -1,6 +1,20 @@
 from datetime import datetime
 
 class Transaction:
+    """
+    A class used to represent a Transaction.
+
+    Attributes
+    ----------
+    client_name : int
+        the name of the client who made the transaction
+    transaction_type : str
+        the type of transaction (withdraw/deposit)
+    transaction_amount : float
+        the amount of transaction
+    transaction_date : datetime
+        the transaction date
+    """
 
     def __init__(
             self,
@@ -8,6 +22,18 @@ class Transaction:
             transaction_type: str,
             transaction_amount: float
     ) -> None:
+        """
+        Initializes transaction.
+
+        Parameters
+        ----------
+        client_name : str
+            The name of the client who made the transaction.
+        transaction_type : str
+            The type of transaction (withdraw/deposit)
+        transaction_amount : float
+            The amount of the transaction.
+        """
         self.client_name = client_name
         self.transaction_type = transaction_type
         self.transaction_amount = transaction_amount
